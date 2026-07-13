682d21c3569c7ec1632bb76b582db240f899f86b
Rishabh-1803 <rishabh180306@gmail.com>
2026-07-13 23:03:16 +0530

    Assignment - 2 Done

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,7 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat ../conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -48,13 +48,15 @@ then
 		exit 1
 	fi
 fi
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
+echo "Removing the old writer utility and compiling as a native application"
+# Clean any previous build artifacts (a cross-compiled writer may be present),
+# then build writer for the native platform by not specifying CROSS_COMPILE.
+make clean
+make
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
